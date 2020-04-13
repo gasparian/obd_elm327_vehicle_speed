36620f95a6a7a90173c6f75bd924e4e6a54891f2
gas <gasparian>
2020-04-13 23:50:30 +0300

    buffer string parsing problem solved

--- src/elm_main.c
+++ src/elm_main.c
@@ -1,86 +1,86 @@
 #include "obd_helpers.h"
 
 void main( int argc, char** argv ) {
-	setbuf(stdout, NULL); // disable stdout buffering
+    setbuf(stdout, NULL); // disable stdout buffering
 
-	printf("\n +----------------------------------------+");
-	printf("\n |         Serial Port Read/Write         |");
-	printf("\n +----------------------------------------+");
+    printf("\n +----------------------------------------+");
+    printf("\n |         Serial Port Read/Write         |");
+    printf("\n +----------------------------------------+");
 
-	/*------------------------ Set initial params --------------------*/
-	int fd; /*File Descriptor*/
-	size_t buff_size = 20; // nbytes, 11 for device check, 2 for speed (?)
+    /*------------------------ Set initial params --------------------*/
+    int fd; /*File Descriptor*/
+    size_t buff_size = 20; // nbytes, 11 for device check, 2 for speed (?)
     size_t vmin = 20; // min characters to read
-	size_t vtime = 8; // in deciseconds
-	char *device_name = "/dev/ttyUSB0";	
-	if (argc > 1) {
-		device_name = argv[1];
-	}
-	/*----------------------------------------------------------------*/
+    size_t vtime = 8; // in deciseconds
+    char *device_name = "/dev/ttyUSB0";    
+    if (argc > 1) {
+        device_name = argv[1];
+    }
+    /*----------------------------------------------------------------*/
 
     /*----------------------- Configure serial port ------------------*/
-	// open serial port file
-	get_port(&fd, &device_name);
+    // open serial port file
+    get_port(&fd, &device_name);
 
-	// set up serial port
-	serial_setup(&fd, vmin, vtime);
-	/*----------------------------------------------------------------*/
+    // set up serial port
+    serial_setup(&fd, vmin, vtime);
+    /*----------------------------------------------------------------*/
 
-	/*----------------------- Talking to the device ------------------*/
-	char *answer = malloc(buff_size);
-	size_t bytes_read = 0;
-	int speed = 0;
-	unsigned long ts;
-	// warming up
-	bytes_read = elm_talk(&fd, answer, buff_size, DEVICE_INFO);
-	int check = answer_check(answer, "ELM327 v1.5", bytes_read);
-	if (check != 0) {
-		fprintf(stderr, "Elm bad response!\n");
-		close(fd); // Close serial port
-		exit(1);
-	}
+    /*----------------------- Talking to the device ------------------*/
+    char *answer = malloc(buff_size);
+    size_t bytes_read = 0;
+    int speed = 0;
+    unsigned long ts;
+    // warming up
+    bytes_read = elm_talk(&fd, answer, buff_size, DEVICE_INFO);
+    int check = answer_check(answer, "ELM327 v1.5", bytes_read);
+    if (check != 0) {
+        fprintf(stderr, "Elm bad response!\n");
+        close(fd); // Close serial port
+        exit(1);
+    }
 
-	printf("\nReady to talk!\n");
-	int iter = 0;
-	while (++iter) {
-		// clean the buff array
-		bzero(answer, buff_size);
+    printf("\nReady to talk!\n");
+    int iter = 0;
+    while (++iter) {
+        // clean the buff array
+        bzero(answer, buff_size);
 
-		// write command and read result
-		bytes_read = elm_talk(&fd, answer, buff_size, DEVICE_INFO); // for debugging without a car --------------------------------
-		// bytes_read = elm_talk(&fd, answer, buff_size, PID_SPEED); // for `prod` ++++++++++++++++++++++++++
-		ts = get_time();
+        // write command and read result
+        bytes_read = elm_talk(&fd, answer, buff_size, DEVICE_INFO); // for debugging without a car --------------------------------
+        // bytes_read = elm_talk(&fd, answer, buff_size, PID_SPEED); // for `prod` ++++++++++++++++++++++++++
+        ts = get_time();
 
-		
-		// int check = answer_check("41 0D 00", "41 0D", 8); // ++++++++++++++++++++++++++
-		// if ( (bytes_read < 0) || (check != 0) ) {
-		// 	printf("Speed reading error!\n");
-		// 	continue;
-		// }
+        
+        // int check = answer_check("41 0D 00", "41 0D", 8); // ++++++++++++++++++++++++++
+        // if ( (bytes_read < 0) || (check != 0) ) {
+        //     printf("Speed reading error!\n");
+        //     continue;
+        // }
 
-		if (bytes_read < 0) { // ----------------------------------
-			printf("Reading error!");
-			continue;
-		}
+        if (bytes_read < 0) { // ----------------------------------
+            printf("Reading error!");
+            continue;
+        }
 
         // DEBUG // ----------------------------------
-		printf("\n[%d] time (us): %lu; Bytes Rxed: %zu; \nAnswer: \n", 
-		       iter-1, ts, bytes_read);
-		printf("%s", answer);
-		printf("\n");
-		//
+        printf("\n[%d] time (us): %lu; Bytes Rxed: %zu; \nAnswer: \n", 
+               iter-1, ts, bytes_read);
+        printf("%s", answer);
+        printf("\n");
+        //
 
-		// usleep(1000000); // sleep for 1 sec.
-		
-		// get the vehicle speed from string ++++++++++++++++++++++++++
-		// speed = get_vehicle_speed("41 0D FF");
-	    // printf("\n[%d] time (us): %lu; Bytes Rxed: %zu; Speed: %d\n", 
-		//        iter-1, ts, bytes_read, speed);
-		
-	}
-	free(answer);
-	/*----------------------------------------------------------------*/
+        // usleep(1000000); // sleep for 1 sec.
+        
+        // get the vehicle speed from string ++++++++++++++++++++++++++
+        // speed = get_vehicle_speed("41 0D FF");
+        // printf("\n[%d] time (us): %lu; Bytes Rxed: %zu; Speed: %d\n", 
+        //        iter-1, ts, bytes_read, speed);
+        
+    }
+    free(answer);
+    /*----------------------------------------------------------------*/
 
-	close(fd); // Close serial port
-	printf("\n +----------------------------------+\n\n\n");
+    close(fd); // Close serial port
+    printf("\n +----------------------------------+\n\n\n");
 }
--- src/obd_helpers.h
+++ src/obd_helpers.h
@@ -15,47 +15,47 @@
 #include <inttypes.h>
 
 void get_port(int *fd, char **device) {
-	/*------------------------------- Opening the Serial Port ---------------------------------*/
+    /*------------------------------- Opening the Serial Port ---------------------------------*/
 
-	*fd = open(*device, O_RDWR | O_NOCTTY);	/* O_RDWR   - Read/Write access to serial port     */
-										    /* O_NOCTTY - No terminal will control the process */
-											/* Open in blocking mode, read will wait           */
-	if(*fd < 0) {	 
-		printf("\n  Error! Cannot open %s \n", *device);
-		exit(1);
-	} 
-	printf("\n %s Opened Successfully! \n", *device);
+    *fd = open(*device, O_RDWR | O_NOCTTY);    /* O_RDWR   - Read/Write access to serial port     */
+                                            /* O_NOCTTY - No terminal will control the process */
+                                            /* Open in blocking mode, read will wait           */
+    if(*fd < 0) {     
+        printf("\n  Error! Cannot open %s \n", *device);
+        exit(1);
+    } 
+    printf("\n %s Opened Successfully! \n", *device);
 }
 
 void serial_setup(int *fd, size_t vmin, size_t vtime) {
-	/*---------- Setting the Attributes of the serial port using termios structure ------------*/
-	
-	struct termios SerialPortSettings;	// Create the structure
+    /*---------- Setting the Attributes of the serial port using termios structure ------------*/
+    
+    struct termios SerialPortSettings;    // Create the structure
 
-	tcgetattr(*fd, &SerialPortSettings); // Get the current attributes of the Serial port
+    tcgetattr(*fd, &SerialPortSettings); // Get the current attributes of the Serial port
 
-	// control options
+    // control options
     // Setting the Baud rate for reading and writing
-	cfsetispeed(&SerialPortSettings, B38400);
-	cfsetospeed(&SerialPortSettings, B38400);
+    cfsetispeed(&SerialPortSettings, B38400);
+    cfsetospeed(&SerialPortSettings, B38400);
 
     // 8N1 - no parity mode
     SerialPortSettings.c_cflag &= ~PARENB;     // Disables the Parity Enable bit(PARENB), So No Parity
-	SerialPortSettings.c_cflag &= ~CSTOPB;     // CSTOPB = 2 Stop bits, here it is cleared so 1 Stop bit
-	SerialPortSettings.c_cflag &= ~CSIZE;      // Clears the mask for setting the data size
-	SerialPortSettings.c_cflag |=  CS8;        // Set the data bits = 8
+    SerialPortSettings.c_cflag &= ~CSTOPB;     // CSTOPB = 2 Stop bits, here it is cleared so 1 Stop bit
+    SerialPortSettings.c_cflag &= ~CSIZE;      // Clears the mask for setting the data size
+    SerialPortSettings.c_cflag |=  CS8;        // Set the data bits = 8
     //
-	SerialPortSettings.c_cflag &= ~CRTSCTS; // No Hardware flow Control
+    SerialPortSettings.c_cflag &= ~CRTSCTS; // No Hardware flow Control
     SerialPortSettings.c_cflag |= CREAD;
     SerialPortSettings.c_cflag |= CLOCAL;
     SerialPortSettings.c_cflag &= ~HUPCL;
     // Setting blocking params
-	SerialPortSettings.c_cc[VMIN] = vmin;   // character count 0-255
-	SerialPortSettings.c_cc[VTIME] = vtime; // in deciseconds (0.1 sec.); if == 0 - Wait indefinetly
+    SerialPortSettings.c_cc[VMIN] = vmin;   // character count 0-255
+    SerialPortSettings.c_cc[VTIME] = vtime; // in deciseconds (0.1 sec.); if == 0 - Wait indefinetly
 
     // line options
     SerialPortSettings.c_lflag |= ECHOE;
-	SerialPortSettings.c_lflag &= ~(ICANON | ECHO | ISIG);
+    SerialPortSettings.c_lflag &= ~(ICANON | ECHO | ISIG);
     // SerialPortSettings.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);  // Non Cannonical mode
 
     // input options
@@ -67,16 +67,16 @@ void serial_setup(int *fd, size_t vmin, size_t vtime) {
     SerialPortSettings.c_iflag &= ~ICRNL;
 
     // Output options
-	SerialPortSettings.c_oflag &= ~OPOST; // No Output Processing
+    SerialPortSettings.c_oflag &= ~OPOST; // No Output Processing
     SerialPortSettings.c_oflag &= ~ONLCR;
 
     // Set the attributes to the termios structure
-	if((tcsetattr(*fd, TCSANOW, &SerialPortSettings)) != 0) { 
-		printf("\n  ERROR ! in Setting attributes");
-		exit(1);
-	} else {
-		printf("\n  Parameters set:\n    BaudRate = 38400 \n    StopBits = 1 \n    Parity = none \n    VMIN = %zu \n    VTIME = %zu \n", vmin, vtime);
-	}
+    if((tcsetattr(*fd, TCSANOW, &SerialPortSettings)) != 0) { 
+        printf("\n  ERROR ! in Setting attributes");
+        exit(1);
+    } else {
+        printf("\n  Parameters set:\n    BaudRate = 38400 \n    StopBits = 1 \n    Parity = none \n    VMIN = %zu \n    VTIME = %zu \n", vmin, vtime);
+    }
 }
 
 void slice_str(char *buffer, const char *str, size_t start, size_t end) {
